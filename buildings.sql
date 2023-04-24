ab8febc2fbd7f213404c39047926e3040448e544
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-23 23:20:29 -0700

    Update buildings.sql

--- buildings.sql
+++ buildings.sql
@@ -1,1 +1,12 @@
+CREATE TABLE buildings (
+  name VARCHAR(80) NOT NULL,
+  shortname VARCHAR(10) UNIQUE,
+  id INTEGER PRIMARY KEY AUTOINCREMENT
+);
 
+CREATE TABLE rooms (
+  number INTEGER,
+  buildingid INTEGER,
+  seating INTEGER,
+  FOREIGN KEY(buildingid) REFERENCES buildings(id)
+);
