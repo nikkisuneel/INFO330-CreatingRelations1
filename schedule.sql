1a7be56c45030c8ae4fe7b0a1c5ff27fe35dfab8
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-23 23:49:27 -0700

    Create schedule.sql

---
+++ schedule.sql
@@ -0,0 +1,13 @@
+CREATE TABLE schedule (
+    id         INTEGER PRIMARY KEY,
+    coursecode TEXT    NOT NULL
+                       REFERENCES courses (coursecode),
+    roomid     INTEGER NOT NULL
+                       REFERENCES room (id),
+    timeslotid INTEGER NOT NULL
+                       REFERENCES timeslots (id),
+    UNIQUE (
+        roomid,
+        timeslotid
+    )
+);
