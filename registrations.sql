8ffc4fdf1097a419b6651a7f99f9c11655233199
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-23 23:22:24 -0700

    Update registrations.sql

--- registrations.sql
+++ registrations.sql
@@ -1,8 +1,4 @@
 CREATE TABLE student_courses (studentid INTEGER REFERENCES students(id), course TEXT REFERENCES courses(code), grade FLOAT DEFAULT NULL);
-INSERT INTO courses (code, start, end, description)
-VALUES ('INFO330A', '2023-04-01', '2023-06-01', 'Data and databases'),
-       ('INFO314', '2023-04-01', '2023-06-01', 'Networking and distributed Systems'),
-       ('INFO448A', '2023-09-25', '2023-12-19', 'Introduction to iOS'),
-       ('INFO449A', '2023-09-25', '2023-12-19', 'Introduction to Android'),
-       ('BAW0100', '2023-04-01', '2023-06-01', 'Introduction to Basket-Weaving'),
-       ('BAW100A', '2023-04-01', '2023-06-01', 'Underwater Basket-Weaving');
+INSERT INTO student_courses (studentid, course)
+   ...> VALUES ((SELECT id FROM students WHERE firstname='Fred'), 'INFO330A'),((SELECT id FROM students WHERE firstname='Fred'), 'INFO448A'), ((SELECT id FROM students WHERE firstname='Fred'), 'INFO314'), ((SELECT id FROM students WHERE firstname='Barney'), 'INFO330A'), ((SELECT id FROM students WHERE firstname='Barney'), 'INFO449A'), ((SELECT id FROM students WHERE firstname='Wilma'), 'BAW0100'), ((SELECT id FROM students WHERE firstname='Wilma'), 'BAW100A'), ((SELECT id FROM students WHERE firstname='Betty'), 'BAW0100');
+sqlite> 
