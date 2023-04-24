8f8571ce077be34d2a399bdbe51efd82d15289b5
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-23 23:28:01 -0700

    Update buildings.sql

--- buildings.sql
+++ buildings.sql
@@ -4,9 +4,21 @@ CREATE TABLE buildings (
   id INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
+INSERT INTO buildings (name, shortname) VALUES ('Mary Gates Hall', 'MGH');
+INSERT INTO buildings (name, shortname) VALUES ('Johnson Hall', 'JHN');
+INSERT INTO buildings (name, shortname) VALUES ('Guggenheim Hall', 'GWN');
+INSERT INTO buildings (name, shortname) VALUES ('Johnson Hall', 'JHN');
+
 CREATE TABLE rooms (
   number INTEGER,
   buildingid INTEGER,
   seating INTEGER,
   FOREIGN KEY(buildingid) REFERENCES buildings(id)
 );
+
+INSERT INTO rooms (number, buildingid, seating) VALUES (400, 2, 40);
+INSERT INTO rooms (number, buildingid, seating) VALUES (500, 2, 50);
+INSERT INTO rooms (number, buildingid, seating) VALUES (700, 2, 70);
+INSERT INTO rooms (number, buildingid, seating) VALUES (600, 2, 60);
+INSERT INTO rooms (number, buildingid, seating) VALUES (400, 2, 40);
+INSERT INTO rooms (number, buildingid, seating) VALUES (400, 1, 40);
